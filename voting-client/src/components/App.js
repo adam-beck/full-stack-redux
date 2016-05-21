5768b5b3d8773a35477b7efb46e6ef43c5bacedd
Adam Beck <AdamB.Infinity@gmail.com>
2016-05-21 13:26:54 -0400

    Get data from redux into react

--- voting-client/src/components/App.js
+++ voting-client/src/components/App.js
@@ -1,11 +1,4 @@
-import React, { Component } from 'react';
-import { List, Map } from 'immutable';
+import React from 'react';
 
-const pair = List.of('Trainspotting', '28 Days Later');
-const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
-
-export default class App extends Component {
-  render() {
-    return React.cloneElement(this.props.children, { pair, tally });
-  }
-};
+const App = ({children}) => children;
+export default App;
